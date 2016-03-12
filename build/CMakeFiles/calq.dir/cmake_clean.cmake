9930f8bbf2e0ef526361d328cd89ac9babb5484a
Jan Voges <voges@tnt.uni-hannover.de>
2016-03-12 20:48:55 +0100

    added CMakeLists.txt and calq.kdev4; moved libosro files to folder src/
    Former-commit-id: 979545e837b9f8141995839c391eaecc6097d440

---
+++ build/CMakeFiles/calq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calq.dir/main.cpp.o"
+  "calq.pdb"
+  "calq"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
