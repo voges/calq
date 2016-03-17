e9e55125c227765bc416a2a8f9126c35764ce00e
Jan Voges <voges@tnt.uni-hannover.de>
2016-03-17 12:16:37 +0100

    improved CMakeLists.txt; wrote some dummies to file predictor.c to suppress compiler warnings
    Former-commit-id: b8f54228ee044df6a1ae300a0c3899d9c6b954f4

--- src/qualcodec/predictor.c
+++ src/qualcodec/predictor.c
@@ -1,5 +1,6 @@
 #include "predictor.h"
 #include "cqlib.h"
+#include <string.h>
 
 static void init(predictor_t *predictor, const size_t memory_sz, const size_t alphabet_sz)
 {
@@ -10,7 +11,7 @@ static void init(predictor_t *predictor, const size_t memory_sz, const size_t al
 predictor_t * predictor_new(const size_t memory_sz, const size_t alphabet_sz)
 {
     predictor_t *predictor = (predictor_t *)cq_malloc(sizeof(predictor_t));
-    predictor->table = (uint32_t *)cq_calloc(memory_sz * alphabet_sz, sizeof(uint32_t));
+    predictor->table = (uint32_t *)cq_calloc(memory_sz * alphabet_sz, sizeof(*(predictor->table)));
     init(predictor, memory_sz, alphabet_sz);
     return predictor;
 }
@@ -28,16 +29,18 @@ void predictor_free(predictor_t *predictor)
 
 void predictor_update(predictor_t *predictor, int *memory, int symbol)
 {
-    
+    predictor->table[*memory+symbol]++; // dummy to prevent compiler warnings
 }
 
 int predictor_predict(predictor_t *predictor, int *memory)
 {
-    
+    int ret = 0;
+    ret = (int)predictor->table[*memory]; // dummy to prevent compiler warnings
+    return ret;
 }
 
 void predictor_reset(predictor_t *predictor)
 {
-    
+    memset(predictor->table, 0x00, predictor->memory_sz * predictor->alphabet_sz * sizeof(*(predictor->table)));
 }
 
