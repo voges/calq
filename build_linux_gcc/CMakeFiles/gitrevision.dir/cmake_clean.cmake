5ef89030aaabce02f8a1c9273c1b2b431cff5793
Jan Voges <voges@tnt.uni-hannover.de>
2016-05-25 14:52:21 +0200

    added folder build_linux_gcc
    Former-commit-id: 802f6156cd40a0f57a461289eab1350be25f2845

---
+++ build_linux_gcc/CMakeFiles/gitrevision.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitrevision"
+  "gitrevision.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gitrevision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_linux_gcc/CMakeFiles/timestamp.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/timestamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
