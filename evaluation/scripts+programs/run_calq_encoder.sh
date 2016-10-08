248805fb9aa2d054f715f04774489c3543ecf1cb
Jan Voges <voges@tnt.uni-hannover.de>
2016-10-08 19:34:43 +0200

    Added bash script to run calq
    Former-commit-id: e54e89065635bce8cc046f40863b14322e628f46

---
+++ evaluation/scripts+programs/run_calq_encoder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 infile blocksize polyploidy qvtype"
+    exit -1
+fi
+
+calq="/data/gidb/MPEG/calq"
+
+date;
+/usr/bin/time -v -o $1.cq.time $calq -b $2 -f -p $3 -t $4 -v $1 -o $1.cq | tee $1.cq.tee
+date;
+echo "Wrote timing info to $1.cq.time"
+echo "Wrote log to $1.cq.tee"
+
