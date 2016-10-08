6ba59a49407fce436d37cedd2533bea915694836
Jan Voges <voges@tnt.uni-hannover.de>
2016-10-08 19:06:51 +0200

    Small bugfixes in evaluation scripts
    Former-commit-id: ee60ea601d8e9f2f4568d0c7befe60b427b61e39

--- evaluation/scripts+programs/happy+reppy.sh
+++ evaluation/scripts+programs/happy+reppy.sh
@@ -7,20 +7,22 @@
 #              reppy (https://github.com/ga4gh/benchmarking-tools/)           #
 ###############################################################################
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 giab|platinum variants.vcf ref.fasta"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 num_threads giab|platinum method variants.vcf ref.fasta"
     exit -1
 fi
 
-compare=$1
-variants_VCF=$2
-ref_FASTA=$3
+num_threads=$1
+compare=$2
+method=$3
+variants_VCF=$4
+ref_FASTA=$5
 
 ### Ground truth
-if [ compare == "giab" ]; then
+if [ $compare == "giab" ]; then
     ground_truth_VCF_GZ="/data/gidb/variant_gold_standards/GIAB-NA12878_HB001-NISTv3.2.2/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.vcf.gz"
     ground_truth_BED="/data/gidb/variant_gold_standards/GIAB-NA12878_HB001-NISTv3.2.2/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.bed"
-elif [ compare == "platinum" ]; then
+elif [ $compare == "platinum" ]; then
     ground_truth_VCF_GZ="/data/gidb/svariant_gold_standards/Illumina_Platinum_Genomes-hg19-8.0.1-NA12878/NA12878.vcf.gz"
     ground_truth_BED="/data/gidb/variant_gold_standards/Illumina_Platinum_Genomes-hg19-8.0.1-NA12878/ConfidentRegions.bed"
 else
@@ -42,9 +44,9 @@ reppy_HTML=$base".reppy.html"
 python $hap_py --threads $num_threads --verbose $ground_truth_VCF_GZ $variants_VCF -f $ground_truth_BED -o $happy_root -r $ref_FASTA --roc VQLSOD
 
 ### Run rep.py
-#rm -f "$happy_root".rep.tsv
+rm -f "$happy_root".rep.tsv
 printf "method\tcomparisonmethod\tfiles\n" >> "$happy_root".rep.tsv
-printf "gatk3\t$compare\t" >> "$happy_root".rep.tsv
+printf "$method\t$compare\t" >> "$happy_root".rep.tsv
 printf "$happy_root.extended.csv," >> "$happy_root".rep.tsv
 for i in "$happy_root".roc.Locations.*; do printf "$i,"; done >> "$happy_root".rep.tsv
 sed -i '$ s/.$//' "$happy_root".rep.tsv
