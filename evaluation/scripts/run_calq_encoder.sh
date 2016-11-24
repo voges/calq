da3b1332cd1abbe83a11cf76de563a3dccd96035
Jan Voges <voges@tnt.uni-hannover.de>
2016-11-24 09:21:45 +0100

    Work in progress
    Former-commit-id: f1fea97805a6fb001b7c25ee4fad1757633f55f4

--- evaluation/scripts/run_calq_encoder.sh
+++ evaluation/scripts/run_calq_encoder.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 infile blocksize polyploidy qvtype"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 infile blocksize polyploidy"
     exit -1
 fi
 
 calq="/data/gidb/MPEG/calq"
 
 date;
-/usr/bin/time -v -o $1.cq.time $calq -b $2 -f -p $3 -t $4 -v $1 -o $1.cq 2>&1 | tee $1.cq.tee
+/usr/bin/time -v -o $1.cq.time $calq -b $2 -f -p $3 -v $1 -o $1.cq 2>&1 | tee $1.cq.tee
 date;
 echo "Wrote timing info to $1.cq.time"
 echo "Wrote log to $1.cq.tee"
