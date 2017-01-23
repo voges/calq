d6fd402f04c59bdc072132359c02437c572a67eb
Jan Voges <voges@tnt.uni-hannover.de>
2017-01-23 17:11:11 +0100

    Fixed evaluation scripts; added license
    Former-commit-id: 5fd9a14fc98e41ea2f2d1a01154cdc72a4f4dfcd

--- evaluation/scripts/run_quartz_paired.sh
+++ evaluation/scripts/run_quartz_paired.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -x
 /project/dna/prog/quartz-0.2.2/quartz /project/dna/resources/quartz_dictionary/dec200.bin.sorted "quartz-0.2.2" 8 0 $1_1.fastq $1_2.fastq 2>&1 | tee $1.quartz-0.2.2.enc.log
-mv $1.filtered_quartz-0.2.2 $1.quartz-0.2.2.fastq
-/home/voges/git/calq/evaluation/scripts/xtract_qual_fastq.py $1.quartz-0.2.2.fastq 2> $1.quartz-0.2.2.fastq.qual
-bzip2 $1.quartz-0.2.2.fastq.qual
-wc -c $1.quartz-0.2.2.fastq.qual.bz2
+mv $1_1.fastq.filtered_quartz-0.2.2 $1_1.fastq.quartz-0.2.2.fastq
+mv $1_2.fastq.filtered_quartz-0.2.2 $1_2.fastq.quartz-0.2.2.fastq
+/home/voges/git/calq/evaluation/scripts/xtract_qual_fastq.py $1_1.fastq.quartz-0.2.2.fastq 2> $1_1.fastq.quartz-0.2.2.fastq.qual
+/home/voges/git/calq/evaluation/scripts/xtract_qual_fastq.py $1_2.fastq.quartz-0.2.2.fastq 2> $1_2.fastq.quartz-0.2.2.fastq.qual
+bzip2 $1_1.fastq.quartz-0.2.2.fastq.qual
+bzip2 $1_2.fastq.quartz-0.2.2.fastq.qual
+wc -c $1_1.fastq.quartz-0.2.2.fastq.qual.bz2
+wc -c $1_2.fastq.quartz-0.2.2.fastq.qual.bz2
--- evaluation/scripts/run_qvz2.sh
+++ evaluation/scripts/run_qvz2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 /home/voges/git/calq/evaluation/scripts/xtract_qual_sam.py $1 2> $1.qual
-/project/dna/prog/qvz2-d5383c6/qvz2 -t $2 -v -u $1.qvz2-d5383c6.qual $1.qual $1.qvz2-d5383c6 2>&1 | tee $1.qvz2-d5383c6.enc.log
-/home/voges/git/calq/evaluation/scripts/replace_qual_sam.py $1 $1.qvz2-d5383c6.qual
-mv $1.new_qual.sam $1.qvz2-d5383c6.sam
+/project/dna/prog/qvz2-d5383c6/qvz2 -t $2 -v -u $1.qvz2-d5383c6-t$2.qual $1.qual $1.qvz2-d5383c6-t$2 2>&1 | tee $1.qvz2-d5383c6-t$2.enc.log
+/home/voges/git/calq/evaluation/scripts/replace_qual_sam.py $1 $1.qvz2-d5383c6-t$2.qual
+mv $1.new_qual.sam $1.qvz2-d5383c6-t$2.sam
