21b215a6552bc67469c7997fcb2bae5d893407ac
Jan Voges <voges@tnt.uni-hannover.de>
2017-01-24 11:09:12 +0100

    Added polyploidy option to run_calq.sh

--- evaluation/scripts/run_calq.sh
+++ evaluation/scripts/run_calq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-/project/dna/bin/calq-d3965e2 -q Illumina-1.8+ -p 1 -b 10000 -f $1 -o $1.cq 2>&1 | tee $1.calq-d3965e2.enc.log
+/project/dna/bin/calq-d3965e2 -q Illumina-1.8+ -p $2 -b 10000 -f $1 -o $1.cq 2>&1 | tee $1.calq-d3965e2.enc.log
 /project/dna/bin/calq-d3965e2 -d -s $1 -f $1.cq 2>&1 | tee $1.calq-d3965e2.dec.log
 mv $1.cq $1.calq-d3965e2
 mv $1.cq.qual $1.calq-d3965e2.qual
