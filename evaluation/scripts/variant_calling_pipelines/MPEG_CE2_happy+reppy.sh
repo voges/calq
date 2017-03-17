4c4b3a065cfde4164ba0123e71a12d38486e81c1
Jan Voges <voges@tnt.uni-hannover.de>
2017-03-17 16:04:50 +0100

    Fixed filename of happy+reppy script

--- evaluation/scripts/variant_calling_pipelines/MPEG_CE2_happy+reppy.sh
+++ evaluation/scripts/variant_calling_pipelines/MPEG_CE2_happy+reppy.sh
@@ -2,41 +2,93 @@
 
 ###############################################################################
 #                Script to report variant calling results with                #
-#                 happy (https://github.com/Illumina/hap.py)                  #
+#                hap.py (https://github.com/Illumina/hap.py)                  #
 #                                    and                                      #
-#              reppy (https://github.com/ga4gh/benchmarking-tools/)           #
+#             rep.py (https://github.com/ga4gh/benchmarking-tools/)           #
 ###############################################################################
 
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 num_threads ref.fasta variants.vcf golden.vcf.gz golden.bed"
+###############################################################################
+#                               Command line                                  #
+###############################################################################
+
+if [ "$#" -ne 6 ]; then
+    printf "Usage: $0 num_threads log_txt variants_vcf ref_fasta golden_vcf_gz golden_bed\n"
     exit -1
 fi
 
-set -x
-
 num_threads=$1
-ref_FASTA=$2
-variants_VCF=$3
-golden_VCF_GZ=$4
-golden_BED=$5
+printf "Number of threads: $num_threads\n"
+log_txt=$2
+printf "Log file: $log_txt\n"
+variants_vcf=$3
+printf "Input VCF file: $variants_vcf\n"
+ref_fasta=$4
+printf "Reference FASTA file: $ref_fasta\n"
+golden_vcf_gz=$5
+printf "Golden VCF file (gzipped): $golden_vcf_gz\n"
+golden_bed=$6
+printf "Golden BED file: $golden_bed\n"
 
-### Programs
+printf "Checking log file $log_txt ... "
+if [ -f $log_txt ]; then printf "log file already exists: $log_txt\n"; exit -1; fi
+printf "OK\n"
+
+printf "Checking input VCF file $variants_vcf ... "
+if [ ! -f $variants_vcf ]; then printf "did not find input VCF file: $variants_vcf\n"; exit -1; fi
+printf "OK\n"
+
+printf "Checking reference FASTA file $ref_fasta ... "
+if [ ! -f $ref_fasta ]; then printf "did not find reference FASTA file: $ref_fasta\n"; exit -1; fi
+printf "OK\n"
+
+printf "Checking golden VCF file (gzipped) $golden_vcf_gz ... "
+if [ ! -f $golden_vcf_gz ]; then printf "did not find golden VCF file (gzipped): $golden_vcf_gz\n"; exit -1; fi
+printf "OK\n"
+
+printf "Checking golden BED file $golden_bed ... "
+if [ ! -f $golden_bed ]; then printf "did not find golden BED file: $golden_bed\n"; exit -1; fi
+printf "OK\n"
+
+###############################################################################
+#                                Executables                                  #
+###############################################################################
+
+python="/usr/bin/python"
 hap_py="/project/dna/install/hap.py-0.3.1/bin/hap.py"
 rep_py="/project/dna/install/benchmarking-tools-c458561/reporting/basic/bin/rep.py"
 
-### File names
-base=$(echo $variants_VCF | sed 's/\.[^.]*$//') # strip .vcf
-happy_root=$base".happy"
-reppy_HTML=$base".reppy.html"
+printf "Checking executables ... "
+if [ ! -x $python ]; then printf "did not find $python\n"; exit -1; fi
+if [ ! -e $hap_py ]; then printf "did not find $hap_py\n"; exit -1; fi
+if [ ! -e $rep_py ]; then printf "did not find $rep_py\n"; exit -1; fi
+printf "OK\n"
 
-### Run hap.py
-python $hap_py --threads $num_threads --verbose $golden_VCF_GZ $variants_VCF -f $golden_BED -o $happy_root -r $ref_FASTA --roc VQLSOD
+###############################################################################
+#                 Report results with hap.py and rep.py                       #
+###############################################################################
 
-### Run rep.py
-rm -f "$happy_root".rep.tsv
-printf "method\tcomparisonmethod\tfiles\n" >> "$happy_root".rep.tsv
-printf "$variants_VCF\t$golden_VCF_GZ\t" >> "$happy_root".rep.tsv
-printf "$happy_root.extended.csv," >> "$happy_root".rep.tsv
-for i in "$happy_root".roc.Locations.*; do printf "$i,"; done >> "$happy_root".rep.tsv
-sed -i '$ s/.$//' "$happy_root".rep.tsv
-python $rep_py -o $reppy_HTML -l "$happy_root".rep.tsv
+printf "[1/2] Running hap.py ... "
+$python $hap_py --threads $num_threads --verbose $golden_vcf_gz $variants_vcf -f $golden_bed -o $variants_vcf.happy -r $ref_fasta --roc VQLSOD &>>$log_txt
+printf "OK\n"
+
+printf "[2/2] Running rep.py ... "
+rm -f "$variants_vcf".happy.rep.tsv
+printf "method\tcomparisonmethod\tfiles\n" >> "$variants_vcf".happy.rep.tsv
+printf "$variants_vcf\t$golden_vcf_gz\t" >> "$variants_vcf".happy.rep.tsv
+printf "$variants_vcf.happy.extended.csv," >> "$variants_vcf".happy.rep.tsv
+for i in "$variants_vcf".happy.roc.Locations.*; do printf "$i,"; done >> "$variants_vcf".happy.rep.tsv
+sed -i '$ s/.$//' "$variants_vcf".happy.rep.tsv
+$python $rep_py -o "$variants_vcf".happy.rep.tsv.reppy.html -l "$variants_vcf".happy.rep.tsv &>>$log_txt
+printf "OK\n"
+
+###############################################################################
+#                                   Cleanup                                   #
+###############################################################################
+
+printf "Cleanup ... "
+rm -f $variants_vcf.happy.extended.csv
+rm -f $variants_vcf.happy.metrics.json
+rm -f $variants_vcf.happy.roc.*
+rm -f $variants_vcf.happy.vcf*
+printf "OK\n";
+
