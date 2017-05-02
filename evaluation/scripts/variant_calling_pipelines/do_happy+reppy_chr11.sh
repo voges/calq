d0ceb271988802ed5db5768656743b8fc85d0d43
Jan Voges <voges@tnt.uni-hannover.de>
2017-05-02 16:46:35 +0200

    Fixed typos

--- evaluation/scripts/variant_calling_pipelines/do_happy+reppy_chr11.sh
+++ evaluation/scripts/variant_calling_pipelines/do_happy+reppy_chr11.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash /home/voges/git/calq/evaluation/scripts/variant_calling_pipelines/MPEG_CE2_happy+reppy.sh 4 $1.MPEG_CE2_happy+reppy.log $1 /data/voges/MPEG/GATK_bundle-2.8-b37/human_g1k_v37.fasta /data/voges/MPEG/GIAB-NA12878_HB001-NISTv3.2.2.11/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.vcf.gz /data/voges/MPEG/GIAB-NA12878_HB001-NISTv3.2.2.11/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.bed
+bash /home/voges/git/calq/evaluation/scripts/variant_calling_pipelines/happy+reppy.sh 4 $1.MPEG_CE2_happy+reppy.log $1 /data/voges/MPEG/GATK_bundle-2.8-b37/human_g1k_v37.fasta /data/voges/MPEG/GIAB-NA12878_HB001-NISTv3.2.2.11/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.vcf.gz /data/voges/MPEG/GIAB-NA12878_HB001-NISTv3.2.2.11/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.bed
 
