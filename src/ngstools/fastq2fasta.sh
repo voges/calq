c5e197950bd7ef6c486a7d3c5e683ff184bb34ae
Jan Voges <voges@tnt.uni-hannover.de>
2017-09-20 21:07:48 +0200

    Work in progress

--- src/ngstools/fastq2fasta.sh
+++ src/ngstools/fastq2fasta.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cat $1 | awk 'NR%4==1{printf ">%s\n", substr($0,2)}NR%4==2{print}'
+
+if [ "$#" -ne 1 ]; then printf "Usage: $0 file.[fastq|fq]\n"; exit -1; fi
+input_fastq=$1
+if [ ! -f input_fastq ]; then printf "Error: Input FASTQ file input_fastq is not a regular file.\n"; exit -1; fi
+cat $input_fastq | awk 'NR%4==1{printf ">%s\n", substr($0,2)}NR%4==2{print}'
