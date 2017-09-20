00811eac8c77ba0afa1f2c8d2d37c8674611a136
Jan Voges <voges@tnt.uni-hannover.de>
2017-09-20 21:04:37 +0200

    Work in progress

--- src/ngstools/compute_coverage_sam.sh
+++ src/ngstools/compute_coverage_sam.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ "$#" -ne 1 ]; then printf "Usage: $0 file.sam\n"; exit -1; fi
-input_sam=$0
+input_sam=$1
 if [ ! -f $input_sam ]; then printf "Error: Input SAM file $input_sam is not a regular file.\n"; exit -1; fi
 samtools="/project/dna/install/samtools-1.3/bin/samtools"
 if [ ! -x $samtools ]; then printf "Error: Binary file $samtools is not executable.\n"; exit -1; fi
