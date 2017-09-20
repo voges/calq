48840dab902e70e220be774c97e2f880d794971e
Jan Voges <voges@tnt.uni-hannover.de>
2017-09-20 21:05:25 +0200

    Work in progress

--- src/ngstools/compute_coverage_sam.sh
+++ src/ngstools/compute_coverage_sam.sh
@@ -5,4 +5,4 @@ input_sam=$1
 if [ ! -f $input_sam ]; then printf "Error: Input SAM file $input_sam is not a regular file.\n"; exit -1; fi
 samtools="/project/dna/install/samtools-1.3/bin/samtools"
 if [ ! -x $samtools ]; then printf "Error: Binary file $samtools is not executable.\n"; exit -1; fi
-$amtools depth $1 | awk '{sum+=$3} END { print "Coverage: ",sum/NR }'
+$samtools depth $1 | awk '{sum+=$3} END { print "Coverage: ",sum/NR }'
