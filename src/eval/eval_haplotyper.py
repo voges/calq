cdb09f6fb9408c7a214ce7ae4459cab05d636222
Fabian Müntefering <fabian.muentefering@mail.de>
2018-06-02 18:21:52 +0200

    evaluation

--- src/eval/eval_haplotyper.py
+++ src/eval/eval_haplotyper.py
@@ -1,12 +1,32 @@
+import os.path
+
 basedir = "/data/voges/muenteferi"
-datasets = ["ERP001775/ERR174324.aln_bowtie2.sorted.dupmark.rg.realn.recal",
-            "NA12878_Garvan_replicate_J/NA12878_V2.5_Robot_2.aln_bowtie2.sorted.dupmark.rg.realn.recal",
-            "NA12878-SRX517292/SRR1238539.aln_bowtie2.sorted.dupmark.rg.realn.recal"]
+datasets = [["ERP001775", "ERR174324.aln_bowtie2.sorted.dupmark.rg.realn.recal"],
+            ["NA12878_Garvan_replicate_J", "NA12878_V2.5_Robot_2.aln_bowtie2.sorted.dupmark.rg.realn.recal"],
+            ["NA12878-SRX517292", "SRR1238539.aln_bowtie2.sorted.dupmark.rg.realn.recal"]]
 subsets = ["3", "11", "20"]
+
+filtersize = ["10", "17", "25"]
+filtertype = ["Gauss", "Rectangle"]
+quantizerType = ["Uniform", "Lloyd"]
+
+quantSteps = [["2", "8"], ["4", "4"]]
+
+squashed = ["", "--noSquash"]
 
 for dset in datasets:
     for sset in subsets:
-        file = "{}/{}.{}/".format(basedir, dset, sset)
-        print(file)
+        folder = "{}/{}/{}.{}/".format(basedir, dset[0], dset[1], sset)
+        file = "{}{}.bam".format(folder, dset[1])
+        if os.path.isfile(file):
+            print("existing: " + file)
+        else:
+            print("NOT existing: " + file)
+
+
+
+def evaluate (folder):
+    pass
+
 
 
