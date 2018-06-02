227458947af97bf084546756025728dbd7563740
Fabian Müntefering <fabian.muentefering@mail.de>
2018-06-02 17:59:22 +0200

    evaluation

--- src/eval/eval_haplotyper.py
+++ src/eval/eval_haplotyper.py
@@ -1,7 +1,7 @@
 basedir = "/data/voges/muenteferi"
-datasets = ["ERP001775/ERR174324.aln_bowtie2.sorted.dupmark.rg.realn.recal.",
-            "NA12878_Garvan_replicate_J/NA12878_V2.5_Robot_2.aln_bowtie2.sorted.dupmark.rg.realn.recal.",
-            "NA12878-SRX517292/SRR1238539.aln_bowtie2.sorted.dupmark.rg.realn.recal."]
+datasets = ["ERP001775/ERR174324.aln_bowtie2.sorted.dupmark.rg.realn.recal",
+            "NA12878_Garvan_replicate_J/NA12878_V2.5_Robot_2.aln_bowtie2.sorted.dupmark.rg.realn.recal",
+            "NA12878-SRX517292/SRR1238539.aln_bowtie2.sorted.dupmark.rg.realn.recal"]
 subsets = ["3", "11", "20"]
 
 for dset in datasets:
