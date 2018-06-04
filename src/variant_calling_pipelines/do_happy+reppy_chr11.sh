1cfc6bdfa7c6e623c97c763d2548f3b974c29b30
Fabian Müntefering <muenteferi@tnt.uni-hannover.de>
2018-06-04 21:30:26 +0200

    Hap.py fix

--- src/variant_calling_pipelines/do_happy+reppy_chr11.sh
+++ src/variant_calling_pipelines/do_happy+reppy_chr11.sh
@@ -5,8 +5,8 @@ num_threads=4
 variants_vcf=$1
 log_txt="$variants_vcf.happy+reppy.log"
 ref_fasta="/data/voges/muenteferi/GATK_bundle-2.8-b37/human_g1k_v37.fasta"
-golden_vcf_gz="/data/gidb/MPEG/GIAB-NA12878_HB001-NISTv3.2.2.11/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.vcf.gz"
-golden_bed="/data/gidb/MPEG/GIAB-NA12878_HB001-NISTv3.2.2.11/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.bed"
+golden_vcf_gz="/data/voges/muenteferi/GIAB-NA12878_HB001-NISTv3.2.2/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.vcf.gz"
+golden_bed="/data/voges/muenteferi/GIAB-NA12878_HB001-NISTv3.2.2/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.11.bed"
 
 bash $happy_reppy_sh $num_threads $log_txt $variants_vcf $ref_fasta $golden_vcf_gz $golden_bed
 
