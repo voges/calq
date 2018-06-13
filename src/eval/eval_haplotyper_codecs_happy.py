f98abd7e8ca4c25ea5612e1f1427f81ff10ad8f5
Fabian Müntefering <fabian.muentefering@mail.de>
2018-06-14 00:18:55 +0200

    size

--- src/eval/eval_haplotyper_codecs_happy.py
+++ src/eval/eval_haplotyper_codecs_happy.py
@@ -19,6 +19,8 @@ codecs = [[".sam", ".calq-0b74e3d", ""], [".bam", ".crumble-0.5", "-1"],[".bam",
 basedir = "/data/voges/muenteferi"
 install_path = "/project/dna/install"
 samtools = install_path + "/samtools-1.3/bin/samtools"
+scramble = "/project/dna/bin/scramble"
+cramsize = "/project/dna/bin/cram_size"
 calqPath = "/home/muenteferi/Dokumente/calqBuild/calq"
 referencePath = "/data/voges/muenteferi/GATK_bundle-2.8-b37/human_g1k_v37.fasta"
 replacePath = "/home/muenteferi/Dokumente/calq/src/ngstools/replace_qual_sam.py"
@@ -41,9 +43,6 @@ for dset in datasets:
         folder = "{}/{}/{}.{}".format(basedir, dset[0], dset[1], sset)
         filename = "{}.{}".format(dset[1], sset)
         filepath = "{}/{}".format(folder, filename)
-        if not os.path.isfile(filepath + ".bam"):
-            print("NOT existing: " + filepath + ".bam", flush=True)
-            exit(-1)
         for codec in codecs:
             outfile = "{}{}/{}.{}{}{}{}".format(folder, codec[1],  dset[1], sset, codec[0], codec[1], codec[2])
             cname = codec[1]+codec[2]
@@ -51,6 +50,17 @@ for dset in datasets:
                 cname = ".none"
             if cname == ".quartz-0.2.2.clipped_qual.aln_bowtie2.sorted.dupmark.rg":
                 cname = ".quartz-0.2.2"
+
+            filesize = "0"
+
+            if not os.path.isfile(outfile + ".bam.cram"):
+                CRAM_Command = "{} -r {} {}.bam {}.bam.cram".format(scramble, referencePath, outfile, outfile)
+                os.system(CRAM_Command)
+            else:
+                print("File '{}.bam.cram' exists. Skipping!\n".format(outfile), flush=True)
+
+            Size_Command = "{} {}.bam.cram | grep QS | awk '{print $6}'".format(cramsize, outfile)
+            filesize = os.popen(Size_Command).read()
 
             # Hap.py / rep.py
             for vcf in vcfList:
@@ -132,7 +142,7 @@ for dset in datasets:
                                                                                             truePositive, falseNegative,
                                                                                             falsePositive, unknownCalls,
                                                                                             precision, recall, fScore,
-                                                                                            -1.0))
+                                                                                            filesize))
                 print("\n", flush=True)
 f.close()
 
