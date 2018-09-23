8ae8d9c24ca6bc07dd15fdcc1fad0d88d8ee14a0
Jan Voges <janvoges@me.com>
2018-09-23 10:19:41 -0500

    Move src/ngstools/ to repo 'htstools'

--- src/ngstools/compute_coverage_sam.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [ "$#" -ne 1 ]; then printf "Usage: $0 file.sam\n"; exit -1; fi
-input_sam=$1
-printf "SAM file: $input_sam\n"
-if [ ! -f $input_sam ]; then printf "Error: Input SAM file $input_sam is not a regular file.\n"; exit -1; fi
-samtools="/project/dna/install/samtools-1.3/bin/samtools"
-if [ ! -x $samtools ]; then printf "Error: Binary file $samtools is not executable.\n"; exit -1; fi
-$samtools depth $1 | awk '{sum+=$3} END { print "Coverage: ",sum/NR }'
--- src/ngstools/fastq2fasta.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 1 ]; then printf "Usage: $0 file.[fastq|fq] 1>file.[fasta|fa]\n"; exit -1; fi
-input_fastq=$1
-if [ ! -f $input_fastq ]; then printf "Error: Input FASTQ file $input_fastq is not a regular file.\n"; exit -1; fi
-cat $input_fastq | awk 'NR%4==1{printf ">%s\n", substr($0,2)}NR%4==2{print}'
