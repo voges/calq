e2f47804b28f4939c7e48b211f82d92003736c6f
Jan Voges <janvoges@me.com>
2018-11-25 18:52:13 -0600

    Move scripts to todo/ folder

---
+++ todo/clip_qual_sam.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+
+# Usage
+if len(sys.argv) != 2:
+    sys.exit("Usage: python {} file.sam 1>clipped.sam".format(sys.argv[0]))
+
+# Get SAM file
+sam_file_name = sys.argv[1]
+if not sam_file_name.endswith(".sam"):
+    sys.exit("SAM file name must end with '.sam'")
+sam_file = open(sam_file_name, 'r')
+print("SAM file: {}".format(sam_file_name))
+
+sys.stderr.write("Clipping quality values into Illumina 1.8+ format ([0,41]+33)\n")
+
+# Initialize statistics
+qual_total_cnt = 0
+qual_raise_cnt = 0
+qual_decrease_cnt = 0
+
+# Parse SAM file and clip the quality scores
+while 1:
+    line = sam_file.readline()
+    if not line:
+        break
+    if line[0] != '@':
+        a = line.split('\t')
+        new_line = a[0] + '\t' + a[1] + '\t' + a[2] + '\t' + a[3] + '\t' + a[4] + '\t' + a[5] + '\t'
+        new_line += a[6] + '\t' + a[7] + '\t' + a[8] + '\t' + a[9] + '\t'
+        qual = a[10]
+
+        # Check and clip quality scores
+
+        # Typical ranges:
+        #  Sanger         Phred+33   [0,40]
+        #  Solexa         Solexa+64  [-5,40]
+        #  Illumina 1.3+  Phred+64   [0,40]
+        #  Illumina 1.5+  Phred+64   [0,40] with 0=unused, 1=unused, 2=Read Segment Quality Control Indicator ('B')
+        #  Illumina 1.8+  Phred+33   [0,41]
+
+        # Currently using Illumina 1.8+
+        new_qual = ""
+        for q in qual:
+            qual_total_cnt += 1
+            if ord(q) < 33:
+                q = chr(33)
+                qual_raise_cnt += 1
+            if ord(q) > 33+41:
+                q = chr(33+41)
+                qual_decrease_cnt += 1
+            new_qual += q
+
+        new_line += new_qual
+
+        if len(a) > 11:
+            for i in range(11, len(a)):
+                new_line += '\t' + a[i]
+        else:
+            new_line += '\n'
+
+        sys.stdout.write(new_line)
+
+    else:
+        sys.stdout.write(line)
+
+sys.stderr.write("Clipped {} out of {} quality values\n".format(qual_raise_cnt+qual_decrease_cnt, qual_total_cnt))
+sys.stderr.write("  Raised: {}\n".format(qual_raise_cnt))
+sys.stderr.write("  Decreased: {}\n".format(qual_decrease_cnt))
+
+sys.exit()
---
+++ todo/xtract_chr_vcf.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+###############################################################################
+#                               Command line                                  #
+###############################################################################
+
+if [ "$#" -ne 3 ]; then printf "Usage: $0 file.vcf ref.[fa|fasta] chromosome\n"; exit -1; fi
+
+root=$(echo $1 | sed 's/\.[^.]*$//') # strip .vcf
+ref=$2
+chromosome=$3
+
+printf "Input VCF file: $1\n"
+printf "Reference FASTA file: $ref\n"
+printf "Chromosome: $chromosome\n"
+
+if [ ! -f $1 ]; then printf "Error: Input VCF file $1 is not a regular file.\n"; exit -1; fi
+if [ ! -f $ref ]; then printf "Error: Reference FASTA file $ref is not a regular file.\n"; exit -1; fi
+
+###############################################################################
+#                                Executables                                  #
+###############################################################################
+
+install_path="/project/dna/install"
+GenomeAnalysisTK_jar="$install_path/gatk-3.6/GenomeAnalysisTK.jar"
+java="/usr/bin/java"
+java_opts=""
+samtools="$install_path/samtools-1.3/bin/samtools"
+
+if [ ! -x $GenomeAnalysisTK_jar ]; then printf "Error: JAR file $GenomeAnalysisTK_jar is not executable.\n"; exit -1; fi
+if [ ! -x $java ]; then printf "Error: Binary file $java is not executable.\n"; exit -1; fi
+if [ ! -x $samtools ]; then printf "Error: Binary file $samtools is not executable.\n"; exit -1; fi
+
+###############################################################################
+#                                 Extraction                                  #
+###############################################################################
+
+printf "Creating FASTA index file: $ref.fai\n"
+if [ -f $ref.fai ]; then
+    printf "$ref.fai already exists (not reproducing it)\n"
+else
+    printf "Handing over to Samtools\n"
+    $samtools faidx $2
+    printf "Returned from Samtools\n"
+fi
+
+printf "Extracting\n  chromosome: $chromosome\n  from: $1\n  to: $root.$chromosome.vcf\n"
+if [ -f $root.$chromosome.vcf ]; then
+    printf "$root.$chromosome.vcf already exists (not reproducing it)\n"
+else
+    printf "Handing over to GATK\n"
+    $java $java_opts -jar $GenomeAnalysisTK_jar \
+        -T SelectVariants \
+        -R $ref \
+        -V $root.vcf \
+        -o $root.$chromosome.vcf \
+        -L $chromosome
+    printf "Returned from GATK\n"
+fi
+
+###############################################################################
+#                                   Cleanup                                   #
+###############################################################################
+
+#printf "Cleanup\n"
+#
+printf "Done\n"
+
