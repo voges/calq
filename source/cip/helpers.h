88d7099d17615c6f38a2b20201b5b62c0eaff243
Jan Voges <voges@tnt.uni-hannover.de>
2019-08-23 17:53:56 +0200

    Clean up helpers

--- source/cip/helpers.h
+++ source/cip/helpers.h
@@ -1,30 +1,14 @@
-#ifndef CALQAPP_HELPERS_H_
-#define CALQAPP_HELPERS_H_
+#ifndef CIP_HELPERS_H_
+#define CIP_HELPERS_H_
 
-// -----------------------------------------------------------------------------
-
-#include <functional>
-#include <memory>
 #include <string>
-#include <utility>
-
-// -----------------------------------------------------------------------------
 
 namespace cip {
 
-// -----------------------------------------------------------------------------
-
-std::string currentDateAndTime();
 bool fileExists(const std::string& path);
-std::string fileBaseName(const std::string& path);
+
 std::string fileNameExtension(const std::string& path);
-std::string removeFileNameExtension(const std::string& path);
 
 }  // namespace cip
 
-// -----------------------------------------------------------------------------
-
-#endif  // CALQAPP_HELPERS_H_
-
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
+#endif  // CIP_HELPERS_H_
