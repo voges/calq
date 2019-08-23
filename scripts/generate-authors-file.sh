9a382e2775e77bbe8855f1ec0fd9836d7601d53b
Jan Voges <voges@tnt.uni-hannover.de>
2019-08-23 16:01:39 +0200

    Add generate-authors-file.sh and the generated AUTHORS file itself

---
+++ scripts/generate-authors-file.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Generate an AUTHORS file based on the output of git shortlog. Uses ABC
+# order, strips out leading spaces and numbers, then filters out specific
+# authors.
+
+set -euo pipefail
+
+git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+git shortlog --summary --email \
+  | perl -spe 's/^\s+\d+\s+//' \
+  | sed -e '/^CommitSyncScript.*$/d' \
+  > "${git_root_dir}/AUTHORS"
