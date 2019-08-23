b2d74ac6c0b906fe28c955afc40b58897cd855a7
Jan Voges <voges@tnt.uni-hannover.de>
2019-08-23 16:02:24 +0200

    More concise build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,37 +1,15 @@
 #!/usr/bin/env bash
 
-self="${0}"
-self_name="${self##*/}"
+set -euxo pipefail
 
-datetime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-
-trace() { echo "[${self_name}] [${datetime}] [trace] ${*}"; }
-debug() { echo "[${self_name}] [${datetime}] [debug] ${*}"; }
-info() { echo "[${self_name}] [${datetime}] [info] ${*}"; }
-warning() { echo "[${self_name}] [${datetime}] [warning] ${*}"; }
-error() { echo "[${self_name}] [${datetime}] [error] ${*}" 1>&2; }
-fatal() { echo "[${self_name}] [${datetime}] [fatal] ${*}" 1>&2; }
-
-run() {
-    local rc
-    info "Running: ${*}"
-    "${@}"
-    rc="${?}"
-    if [[ ${rc} != 0 ]]; then
-        fatal "Command failed (RC=${rc}): ${*}"
-        exit ${rc}
-    fi
-}
-
-run git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
 readonly build_dir="${git_root_dir}/build"
 if [[ ! -d "${build_dir}" ]]; then
-    info "Creating directory for CMake build: ${build_dir}"
-    run mkdir -p "${build_dir}"
+    mkdir -p "${build_dir}"
 fi
 
-run cd "${build_dir}"
-run cmake ..
-run make
+cd "${build_dir}"
+cmake ..
+make
