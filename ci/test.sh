03b8f3fe45ca69bd693a8b73f464821c8280ea53
Jan Voges <janvoges@me.com>
2019-08-27 15:18:15 +0200

    Add unit tests to Travis CI

---
+++ ci/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+"${git_root_dir}/build/bin/calq-tests"
