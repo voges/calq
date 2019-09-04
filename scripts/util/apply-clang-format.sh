ac57cb455195753dda87cc338420b9c294206db9
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-04 09:30:12 +0200

    Update apply-clang-format.sh

--- scripts/util/apply-clang-format.sh
+++ scripts/util/apply-clang-format.sh
@@ -29,6 +29,16 @@ for f in "${git_root_dir}"/source/libs/calq/*.cc; do
     clang-format -i "${f}"
 done
 
+for f in "${git_root_dir}"/source/libs/util/*.h; do
+    echo "Running clang-format on: ${f}"
+    clang-format -i "${f}"
+done
+
+for f in "${git_root_dir}"/source/libs/util/*.cc; do
+    echo "Running clang-format on: ${f}"
+    clang-format -i "${f}"
+done
+
 # for f in "${git_root_dir}"/tests/example/*.h; do
 #     echo "Running clang-format on: ${f}"
 #     clang-format -i "${f}"
@@ -45,6 +55,16 @@ done
 # done
 
 for f in "${git_root_dir}"/tests/libs/calq/*.cc; do
+    echo "Running clang-format on: ${f}"
+    clang-format -i "${f}"
+done
+
+# for f in "${git_root_dir}"/tests/libs/util/*.h; do
+#     echo "Running clang-format on: ${f}"
+#     clang-format -i "${f}"
+# done
+
+for f in "${git_root_dir}"/tests/libs/util/*.cc; do
     echo "Running clang-format on: ${f}"
     clang-format -i "${f}"
 done
