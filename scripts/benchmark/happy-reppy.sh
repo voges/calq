2facdf98cc91e1d86d766e70d8f023e82f7cbd2b
Jan Voges <janvoges@me.com>
2019-09-06 14:05:26 +0200

    Update scripts/benchmark/happy-reppy.sh

--- scripts/benchmark/happy-reppy.sh
+++ scripts/benchmark/happy-reppy.sh
@@ -5,6 +5,8 @@
 # and
 #   rep.py (https://github.com/ga4gh/benchmarking-tools/)
 
+set -euo pipefail
+
 readonly python=""
 readonly hap_py=""
 readonly rep_py=""
@@ -14,21 +16,35 @@ if [[ "${#}" -ne 6 ]]; then
     exit 1
 fi
 
-readonly num_threads="$1"
-readonly log_txt="$2"
-readonly variants_vcf="$3"
-readonly ref_fasta="$4"
-readonly golden_vcf_gz="$5"
-readonly golden_bed="$6"
+readonly num_threads="${1}"
+readonly log_txt="${2}"
+readonly variants_vcf="${3}"
+readonly ref_fasta="${4}"
+readonly golden_vcf_gz="${5}"
+readonly golden_bed="${6}"
 
 echo "[1/2] Running hap.py"
-${python} ${hap_py} --verbose --threads ${num_threads} ${golden_vcf_gz} ${variants_vcf} -f ${golden_bed} -o ${variants_vcf}.happy -r ${ref_fasta} --roc VQLSOD &>>${log_txt}
+"${python}" "${hap_py}" \
+    --verbose \
+    --threads "${num_threads}" \
+    "${golden_vcf_gz}" \
+    "${variants_vcf}" \
+    -f ${golden_bed} \
+    -o ${variants_vcf}.happy \
+    -r ${ref_fasta} \
+    --roc VQLSOD \
+    &>>${log_txt}
 
 echo "[2/2] Running rep.py"
-rm -f "${variants_vcf}".happy.rep.tsv
-echo "method\tcomparisonmethod\tfiles" >> "${variants_vcf}".happy.rep.tsv
-echo "${variants_vcf}\t${golden_vcf_gz}\t" >> "${variants_vcf}".happy.rep.tsv
-echo "${variants_vcf}.happy.extended.csv," >> "${variants_vcf}".happy.rep.tsv
-for i in "${variants_vcf}".happy.roc.Locations.*; do echo "$i,"; done >> "${variants_vcf}".happy.rep.tsv
+rm -f "${variants_vcf}.happy.rep.tsv"
+echo "method\tcomparisonmethod\tfiles" >>"${variants_vcf}".happy.rep.tsv
+echo "${variants_vcf}\t${golden_vcf_gz}\t" >>"${variants_vcf}".happy.rep.tsv
+echo "${variants_vcf}.happy.extended.csv," >>"${variants_vcf}".happy.rep.tsv
+for i in "${variants_vcf}".happy.roc.Locations.*; do
+    echo "${i},";
+done >> "${variants_vcf}".happy.rep.tsv
 sed -i '$ s/.$//' "${variants_vcf}".happy.rep.tsv
-${python} ${rep_py} -o "${variants_vcf}".happy.rep.tsv.reppy.html -l "${variants_vcf}".happy.rep.tsv &>>${log_txt}
+"${python}" "${rep_py}" \
+    -o "${variants_vcf}.happy.rep.tsv.reppy.html" \
+    -l "${variants_vcf}.happy.rep.tsv" \
+    &>>${log_txt}
