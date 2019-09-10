1f149ddc55480e3caf414064c517e5a1cf08d20f
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-10 14:53:07 +0200

    Fix memory leak in SamRecord class

--- source/libs/calq/sam-record.cc
+++ source/libs/calq/sam-record.cc
@@ -1,22 +1,40 @@
 #include "sam-record.h"
 #include <queue>
 #include <sstream>
+#include "errors.h"
 
 namespace calq {
 
 SamRecord::SamRecord(const std::vector<std::string> &fields)
-    : qname(fields[0]),
-      flag(static_cast<uint16_t>(std::stoi(fields[1]))),
-      rname(fields[2]),
-      pos(static_cast<uint32_t>(std::stoi(fields[3]))),
-      mapq(static_cast<uint8_t>(std::stoi(fields[4]))),
-      cigar(fields[5]),
-      rnext(fields[6]),
-      pnext(static_cast<uint32_t>(std::stoi(fields[7]))),
-      tlen(static_cast<int64_t>(std::stoi(fields[8]))),
-      seq(fields[9]),
-      qual(fields[10]),
-      opt(fields[11]) {}
+    : qname(""),
+      flag(0),
+      rname(""),
+      pos(0),
+      mapq(0),
+      cigar(""),
+      rnext(""),
+      pnext(0),
+      tlen(0),
+      seq(""),
+      qual(""),
+      opt("") {
+    if (fields.size() != 12) {
+        throwErrorException("fields.size() != 12");
+    }
+
+    qname = fields[0];
+    flag = static_cast<uint16_t>(std::stoi(fields[1]));
+    rname = fields[2];
+    pos = static_cast<uint32_t>(std::stoi(fields[3]));
+    mapq = static_cast<uint8_t>(std::stoi(fields[4]));
+    cigar = fields[5];
+    rnext = fields[6];
+    pnext = static_cast<uint32_t>(std::stoi(fields[7]));
+    tlen = static_cast<int64_t>(std::stoi(fields[8]));
+    seq = fields[9];
+    qual = fields[10];
+    opt = fields[11];
+}
 
 std::string SamRecord::str() const {
     std::stringstream ss;
--- tests/libs/calq/sam-record-test.cc
+++ tests/libs/calq/sam-record-test.cc
@@ -4,29 +4,18 @@
 TEST(SamRecord, String) {  // NOLINT(cert-err58-cpp)
     std::vector<std::string> fields;
 
-    std::string qname = "QNAME";
-    std::string flag = "0";
-    std::string rname = "RNAME";
-    std::string pos = "0";
-    std::string mapq = "0";
-    std::string cigar = "CIGAR";
-    std::string rnext = "RNEXT";
-    std::string pnext = "0";
-    std::string tlen = "0";
-    std::string seq = "SEQ";
-    std::string opt = "OPT";
-
-    fields.push_back(qname);
-    fields.push_back(flag);
-    fields.push_back(rname);
-    fields.push_back(pos);
-    fields.push_back(mapq);
-    fields.push_back(cigar);
-    fields.push_back(rnext);
-    fields.push_back(pnext);
-    fields.push_back(tlen);
-    fields.push_back(seq);
-    fields.push_back(opt);
+    fields.emplace_back("QNAME");
+    fields.emplace_back("0");
+    fields.emplace_back("RNAME");
+    fields.emplace_back("0");
+    fields.emplace_back("0");
+    fields.emplace_back("CIGAR");
+    fields.emplace_back("RNEXT");
+    fields.emplace_back("0");
+    fields.emplace_back("0");
+    fields.emplace_back("SEQ");
+    fields.emplace_back("QUAL");
+    fields.emplace_back("OPT");
 
     calq::SamRecord record(fields);
 }
