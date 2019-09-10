8f33b29ee49269e38423f1c00b0fa1c8c9ca0a13
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-10 18:09:17 +0200

    Update before-commit.sh

--- before-commit.sh
+++ before-commit.sh
@@ -1,62 +1,22 @@
 #!/usr/bin/env bash
 
-# Apply clang-format in-place on all C++ source code files
+# This script performs some routine maintenance and should be executed
+# before every commit.
 
 set -euo pipefail
 
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
-{
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/ci/cmake-build-release.sh"
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/ci/cmake-build-release.sh";
+execute() {
+    echo ""
+    echo "==============================================================================="
+    echo "Running: ${1}"
+    echo "==============================================================================="
+    echo ""
 
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/ci/cmake-build-debug-with-doc-and-coverage.sh";
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/ci/cmake-build-debug-with-doc-and-coverage.sh";
+    "${1}"
+}
 
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/ci/run-all-tests.sh";
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/ci/run-all-tests.sh";
-
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/ci/run-all-tests-with-valgrind.sh";
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/ci/run-all-tests-with-valgrind.sh";
-
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/ci/report-code-coverage.sh";
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/ci/report-code-coverage.sh";
-
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/scripts/util/apply-clang-format.sh";
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/scripts/util/apply-clang-format.sh";
-
-  echo ""
-  echo "==============================================================================="
-  echo "${git_root_dir}/scripts/util/generate-authors-file.sh";
-  echo "==============================================================================="
-  echo ""
-  "${git_root_dir}/scripts/util/generate-authors-file.sh";
-
-} &>"${git_root_dir}/before-commit.sh.log"
-
-echo "Done. Wrote log to: ${git_root_dir}/before-commit.sh.log"
+execute "${git_root_dir}/scripts/util/apply-clang-format.sh"
+execute "${git_root_dir}/scripts/util/generate-authors-file.sh"
