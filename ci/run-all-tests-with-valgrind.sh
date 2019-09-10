4e6e1dd4bd58496443525de08d581ae1a689eaef
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-10 15:28:10 +0200

    Add run-all-tests-with-valgrind.sh

---
+++ ci/run-all-tests-with-valgrind.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+readonly build_dir="${git_root_dir}/cmake-build-debug-with-doc-and-coverage"
+[[ -d "${build_dir}" ]] # exit if build dir does *not* exist
+
+valgrind --tool=memcheck --verbose --track-origins=yes "${build_dir}/bin/calq-tests"
