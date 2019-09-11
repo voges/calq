662ccbff458eb03407a310f3b72ad21070d0d146
Jan Voges <voges@tnt.uni-hannover.de>
2019-09-11 08:37:56 +0200

    Better naming of local variables in file-test.cc

--- tests/libs/calq/file-test.cc
+++ tests/libs/calq/file-test.cc
@@ -37,48 +37,48 @@ TEST(File, AdvanceAndTell) {  // NOLINT(cert-err58-cpp)
 
 TEST(File, Eof) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = calq_tests::exec("git rev-parse --show-toplevel");
-    calq::FileReader reader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
+    calq::FileReader fileReader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
 
     // Without reading or writing from/to a file we do not really have the means to test the eof() function.
-    EXPECT_EQ(reader.eof(), false);
+    EXPECT_EQ(fileReader.eof(), false);
 }
 
 TEST(File, SeekFromCurAndTell) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = calq_tests::exec("git rev-parse --show-toplevel");
-    calq::FileReader reader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
+    calq::FileReader fileReader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
 
-    EXPECT_NO_THROW(reader.seekFromCur(0));
-    EXPECT_EQ(reader.tell(), 0);
+    EXPECT_NO_THROW(fileReader.seekFromCur(0));
+    EXPECT_EQ(fileReader.tell(), 0);
 }
 
 TEST(File, SeekFromEndAndTell) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = calq_tests::exec("git rev-parse --show-toplevel");
-    calq::FileReader reader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
+    calq::FileReader fileReader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
 
-    EXPECT_NO_THROW(reader.seekFromEnd(0));
-    EXPECT_EQ(reader.tell(), 4);
+    EXPECT_NO_THROW(fileReader.seekFromEnd(0));
+    EXPECT_EQ(fileReader.tell(), 4);
 }
 
 TEST(File, SeekFromSetAndTell) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = calq_tests::exec("git rev-parse --show-toplevel");
-    calq::FileReader reader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
+    calq::FileReader fileReader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
 
-    EXPECT_NO_THROW(reader.seekFromSet(0));
-    EXPECT_EQ(reader.tell(), 0);
+    EXPECT_NO_THROW(fileReader.seekFromSet(0));
+    EXPECT_EQ(fileReader.tell(), 0);
 }
 
 TEST(File, Size) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = calq_tests::exec("git rev-parse --show-toplevel");
-    calq::FileReader reader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
+    calq::FileReader fileReader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
 
-    EXPECT_EQ(reader.size(), 4);
+    EXPECT_EQ(fileReader.size(), 4);
 }
 
 TEST(File, Tell) {  // NOLINT(cert-err58-cpp)
     std::string gitRootDir = calq_tests::exec("git rev-parse --show-toplevel");
-    calq::FileReader reader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
+    calq::FileReader fileReader(gitRootDir + "/resources/test-files/tests/0x041f5aac");
 
-    EXPECT_EQ(reader.tell(), 0);
-    reader.advance(1);
-    EXPECT_EQ(reader.tell(), 1);
+    EXPECT_EQ(fileReader.tell(), 0);
+    fileReader.advance(1);
+    EXPECT_EQ(fileReader.tell(), 1);
 }
