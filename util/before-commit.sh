13578c3e66c6c4ae21390783457cef9fc598237b
Jan Voges <janvoges@me.com>
2019-09-17 22:02:44 +0200

    Update util/before-commit.sh

--- util/before-commit.sh
+++ util/before-commit.sh
@@ -5,16 +5,11 @@ set -euo pipefail
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
-execute() {
-    echo ""
-    echo "==============================================================================="
-    echo "Running: ${1}"
-    echo "==============================================================================="
-    echo ""
-
+run() {
+    echo "-- running: ${1}"
     "${1}"
 }
 
-execute "${git_root_dir}/util/clang-format.sh"
-execute "${git_root_dir}/util/authors.sh"
-execute "${git_root_dir}/util/shellcheck.sh"
+run "${git_root_dir}/util/clang-format.sh"
+run "${git_root_dir}/util/authors.sh"
+run "${git_root_dir}/util/shellcheck.sh"
