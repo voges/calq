32264d2a2485cc1b56485364d5461e5ea73600ff
Jan Voges <voges@tnt.uni-hannover.de>
2019-12-05 15:40:07 +0100

    Update utility scripts

--- util/todo.sh
+++ util/todo.sh
@@ -8,17 +8,6 @@ self_name="${self##*/}"
 git rev-parse --git-dir 1>/dev/null # exit if not inside Git repo
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 
-cmds=()
-cmds+=("grep")
-
-for i in "${!cmds[@]}"; do
-    cmd=${cmds[${i}]}
-    if not command -v "${cmd}" &>/dev/null; then
-        echo "[${self_name}] error: command does not exist: ${cmd}"
-        exit 1
-    fi
-done
-
 dirs=()
 dirs+=("${git_root_dir}/src")
 dirs+=("${git_root_dir}/test")
